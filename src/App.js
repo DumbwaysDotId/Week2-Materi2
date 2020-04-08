3b895fb9c12d9fa21c4cee8e468960e0c503ebc1
radifanmp <radifanmp@gmail.com>
2020-04-08 10:12:35 +0700

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Todo from './Todo';
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+
+    this.todoInput = ""
+  }
+
+  addTodo(){
+    let todoValue = this.todoInput.value
+    let newTodos = this.state.todos
+    newTodos.push(todoValue)
+    
+    this.setState({
+      todos: newTodos
+    })
+
+    //Reset Value
+    this.todoInput.value = ""
+
+    //Set Focus to input
+    this.todoInput.focus()
+  }
+
+  removeTodo(id) {
+    let todos = this.state.todos.filter((todo,index) => {
+      return id !== index
+    })
+
+    this.setState({
+      todos: todos
+    })
+  }
+
+  render(){
+    return(
+      <div className = "App-header">
+        <h1>Todo App</h1>
+          <p>Todo's Count : {this.state.todos.length}</p>
+
+        <ul>
+          {this.state.todos.map((todo,index) => {
+            return (<Todo id={index} key={index} todo={todo} onRemove={() => this.removeTodo(index)}/>)
+           } ) }
+        </ul>
+
+        <input type="text" placeholder="Enter Todo" ref={(input) => this.todoInput = input} />
+        <button onClick={this.addTodo.bind(this)}>Add</button>
+      </div>
+    );
+  }
 }
 
 export default App;
